584a0dd9d9e704a41d139f8e64ccfd8984cd7423
Nadhirrezig <rzignadhir56@gmail.com>
2025-01-23 20:39:28 +0100

    dashboard Update

--- app/lib/placeholder-data.ts
+++ app/lib/placeholder-data.ts
@@ -1,8 +1,8 @@
 const users = [
   {
     id: '410544b2-4001-4271-9855-fec4b6a6442a',
-    name: 'User',
-    email: 'user@nextmail.com',
+    name: 'Nadhir',
+    email: 'rzignadhir@gmail.com',
     password: '123456',
   },
 ];
@@ -10,20 +10,20 @@ const users = [
 const customers = [
   {
     id: 'd6e15727-9fe1-4961-8c5b-ea44a9bd81aa',
-    name: 'Evil Rabbit',
-    email: 'evil@rabbit.com',
+    name: 'Nadhir rezig',
+    email: 'rzignadhir@gmail.com',
     image_url: '/customers/evil-rabbit.png',
   },
   {
     id: '3958dc9e-712f-4377-85e9-fec4b6a6442a',
-    name: 'Delba de Oliveira',
-    email: 'delba@oliveira.com',
+    name: 'obama ben ladin',
+    email: 'Maxyab12@gmail.com',
     image_url: '/customers/delba-de-oliveira.png',
   },
   {
     id: '3958dc9e-742f-4377-85e9-fec4b6a6442a',
-    name: 'Lee Robinson',
-    email: 'lee@robinson.com',
+    name: 'manel',
+    email: 'manel08@gmail.com',
     image_url: '/customers/lee-robinson.png',
   },
   {
