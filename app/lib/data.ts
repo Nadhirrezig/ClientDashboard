e73181018be35ccf1b52e8262ff683241714c1fb
Nadhirrezig <rzignadhir56@gmail.com>
2025-01-24 22:44:49 +0100

    new update

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -14,9 +14,12 @@ export async function fetchRevenue() {
     //dont do this please :D its just an artifitial delay
     // why am i drowning
     console.log('Fetching revenue data...');
-    await new Promise((resolve) => setTimeout(resolve, 2000));
+    await new Promise((resolve) => setTimeout(resolve, 3000));
 
     const data = await sql<Revenue>`SELECT * FROM revenue`;
+
+    console.log('Data fetch completed after 3 seconds.');
+
     return data.rows;
   } catch (error) {
     console.error('Database Error:', error);
