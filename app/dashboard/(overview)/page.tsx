09662ad9c632ea7d285f79abcb32843b0f4145ff
Nadhirrezig <rzignadhir56@gmail.com>
2025-01-28 18:15:01 +0100

    issue fixed

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -5,7 +5,6 @@ export const dynamic = 'force-dynamic';
 import RevenueChart from '@/app/ui/dashboard/revenue-chart';
 import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
 import { lusitana } from '@/app/ui/fonts';
-import {fetchCardData } from '@/app/lib/data';
 import { RevenueChartSkeleton, LatestInvoicesSkeleton , CardsSkeleton} from '@/app/ui/skeletons';
 import CardWrapper from '@/app/ui/dashboard/cards';
 import { Suspense } from 'react';
